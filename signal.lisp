34c2b5bac86d62381e45366d7bd29e4158759ebc
t-sin <shinichi.tanaka45@gmail.com>
2019-01-17 02:49:04 +0900

    Basic saw and tri (not tested)

--- signal.lisp
+++ signal.lisp
@@ -1,6 +1,13 @@
 (defpackage #:pukunui/signal
   (:use #:cl)
-  (:export #:pan))
+  (:export #:pan
+
+           #:2PI
+           #:PI/2
+           #:3PI/2
+
+           #:tri
+           #:saw))
 (in-package #:pukunui/signal)
 
 (defun pan (l r pan)
@@ -8,3 +15,18 @@
   (cond ((zerop pan) (values l r))
         ((plusp pan) (values (* (- 1 pan) l) r))
         ((minusp pan) (values l (* (+ 1 pan) r)))))
+
+(defconstant 2PI (* 2 PI))
+(defconstant PI/2 (/ PI 2))
+(defconstant 3PI/2 (* 3 (/ PI 2)))
+
+(defun tri (x)
+  (let ((x (mod x (* 2 PI))))
+    (cond ((>= x 3PI/2) (- (* PI/2 x) 1.5))
+          ((and (<= PI/2 x) (< x 3PI/2)) (+ (* -1 PI/2 x) 1.0))
+          (t (* PI/2 x)))))
+
+(defun saw (x)
+  (let ((x (mod x (* 2 PI))))
+    (cond ((>= x PI) (- (/ x PI) 1)
+          ((t (/ x PI)))))))
