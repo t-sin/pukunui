ddba49c80c81002dc4c38dacb8b1755c31033ecf
t-sin <shinichi.tanaka45@gmail.com>
2019-01-17 14:34:57 +0900

    Simple mixing

--- signal.lisp
+++ signal.lisp
@@ -17,6 +17,13 @@
         ((plusp pan) (values (* (- 1 pan) l) r))
         ((minusp pan) (values l (* (+ 1 pan) r)))))
 
+(defun mix (s1 g1 s2 g2)
+  (+ (* s1 g1) (* s2 g2)))
+
+(defun mix-2 (l1 r1 g1 l2 r2 g2)
+  (values (mix l1 l2 g1 g2)
+          (mix r1 r2 g1 g2)))
+
 (defconstant 2PI (* 2 PI))
 (defconstant PI/2 (/ PI 2))
 (defconstant 3PI/2 (* 3 (/ PI 2)))
