62e6982829e0142ecf34f29330dc5f0b97e1626a
t-sin <shinichi.tanaka45@gmail.com>
2019-01-24 15:47:11 +0900

    Make oscillators stereo

--- units/oscillator.lisp
+++ units/oscillator.lisp
@@ -15,19 +15,19 @@
     (((freq :export) :default 440 :max 20000 :min 0.05 :step 0.01))
   (let ((v (sin (+ (osc-ph u) (calc-slot (osc-init-ph u))))))
     (incf (osc-ph u) (* (/ (calc-slot (sine-freq u)) +sample-rate+) PI))
-    v))
+    (values v v))
 
 (defunit tri (osc)
     (((freq :export) :defaut 440 :max 20000 :min 0.05 :step 0.01))
   (let ((v (tri (+ (osc-ph u) (calc-slot (osc-init-ph u))))))
     (incf (osc-ph u) (/ (calc-slot (tri-freq u)) +sample-rate+ 2))
-    v))
+    (values v v))
 
 (defunit saw (osc)
     (((freq :export) :defaut 440 :max 20000 :min 0.05 :step 0.01))
   (let ((v (saw (+ (osc-ph u) (calc-slot (osc-init-ph u))))))
     (incf (osc-ph u) (/ (calc-slot (saw-freq u)) +sample-rate+ 2))
-    v))
+    (values v v))
 
 (defunit pulse (osc)
     (((freq :export) :defaut 440 :max 20000 :min 0.05 :step 0.01)
@@ -35,4 +35,4 @@
   (let ((v (pulse (+ (osc-ph u) (calc-slot (osc-init-ph u)))
                   (calc-slot (pulse-duty u)))))
     (incf (osc-ph u) (/ (calc-slot (pulse-freq u)) +sample-rate+ 2))
-    v))
+    (values v v))
