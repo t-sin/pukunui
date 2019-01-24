5a91699ed8f205692d0bc43f563ed542218c8d44
t-sin <shinichi.tanaka45@gmail.com>
2019-01-24 15:47:24 +0900

    Indentation

--- units/core.lisp
+++ units/core.lisp
@@ -7,8 +7,8 @@
 (in-package #:pukunui/units/core)
 
 (defunit unit ()
-    (((out :export) :default 0 :max 1 :min -1)
-     ((gain :export) :default 1 :max 1 :min 0))
+  (((out :export) :default 0 :max 1 :min -1)
+   ((gain :export) :default 1 :max 1 :min 0))
   (let ((v (gain (calc-slot (unit-out u))
                  (calc-slot (unit-gain u)))))
     (values v v)))
