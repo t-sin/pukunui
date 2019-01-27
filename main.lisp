39a77d651654f74d90bcc24e8c6eb1732b088318
t-sin <shinichi.tanaka45@gmail.com>
2019-01-28 04:24:45 +0900

    Change samples

--- main.lisp
+++ main.lisp
@@ -18,8 +18,10 @@
   (calc-unit *unit-graph*))
 
 (defun start* ()
-  (let ((clip (read-wav (asdf:system-relative-pathname :pukunui "ev.wav"))))
+  (let ((clip (read-wav (asdf:system-relative-pathname :pukunui "ev.wav")))
+        (gain-mod (create-offset 0.7)))
+    (setf (unit-src gain-mod) (create-sine 39))
     (setf (clip-playing-p clip) t)
     (setf (clip-loop-p clip) t)
-    (setf *unit-graph* (create-unit clip 0.8)))
+    (setf *unit-graph* (create-unit clip gain-mod)))
   (start (make-paconf*) #'calc-toplevel))
