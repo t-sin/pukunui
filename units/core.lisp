54fcd00ce28b2a4178c5a1d377ef60ae14e186bb
t-sin <shinichi.tanaka45@gmail.com>
2019-01-28 21:35:36 +0900

    Replace unit slot access with ref-reader (`#@`)

--- units/core.lisp
+++ units/core.lisp
@@ -10,22 +10,22 @@
   (((src :export) :default 0 :max 1 :min -1)
    ((gain :export) :default 1 :max 1 :min 0))
   (multiple-value-bind (l r)
-      (calc-slot (unit-src u))
-    (let* ((g (calc-slot (unit-gain u)))
+      #@unit-src
+    (let* ((g #@unit-gain)
            (l (gain l g))
            (r (gain r g)))
       (values l r))))
 
 (defunit offset (unit)
   (((value :export) :default 0))
-  (let ((val (calc-slot (offset-value u))))
+  (let ((val #@offset-value))
     (multiple-value-bind (l r)
-        (calc-slot (unit-src u))
+        #@unit-src
     (values (+ l val) (+ r val)))))
 
 (defunit amp (unit)
   (((value :export) :default 1))
-  (let ((val (calc-slot (amp-value u))))
+  (let ((val #@amp-value))
     (multiple-value-bind (l r)
-        (calc-slot (unit-src u))
+        #@unit-src
       (values (* l val) (* r val)))))
