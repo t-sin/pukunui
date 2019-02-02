aa7d5b831b496831e3a3ed58ecb077a299685398
t-sin <shinichi.tanaka45@gmail.com>
2019-02-02 22:39:02 +0900

    Add type for playback mode

--- masterinfo.lisp
+++ masterinfo.lisp
@@ -1,21 +1,27 @@
 (defpackage #:pukunui/masterinfo
   (:use #:cl
         #:pukunui/constants)
-  (:export #:masterinfo
+  (:export #:playback-mode
+           #:masterinfo
            #:make-masterinfo
            #:masterinfo-p
-           #:masterinfo-tick
-           #:masterinfo-bpm
            #:masterinfo-frames-per-buffer
            #:masterinfo-sample-rate
+           #:masterinfo-bpm
+           #:masterinfo-playback-mode
+           #:masterinfo-tick
 
            #:update-masterinfo))
 (in-package #:pukunui/masterinfo)
+
+(deftype playback-mode ()
+  '(member :timeline :clip))
 
 (defstruct masterinfo
   (frames-per-buffer +frames-per-buffer+)
   (sample-rate +sample-rate+)
   (bpm +bpm+)
+  (playback-mode :timeline :type 'playback-mode)
   (tick 0))
 
 (defun update-masterinfo (masterinfo)
