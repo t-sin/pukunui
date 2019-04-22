ee941c2cf1f38d46221e74c99ec1ae7d66b24c35
t-sin <shinichi.tanaka45@gmail.com>
2019-04-22 22:08:47 +0900

    PCM files load only when first time

--- examples/02-pcm.lisp
+++ examples/02-pcm.lisp
@@ -9,31 +9,36 @@
                :pukunui/units/sample
                :pukunui/event))
 
+(defparameter *ev* (read-wav (asdf:system-relative-pathname :pukunui "resouces/ev.wav")))
+(defparameter *dr* (read-wav (asdf:system-relative-pathname :pukunui "resouces/dr.wav")))
+
 ;; play a WAV file Oneshot
-(let ((ev (read-wav (asdf:system-relative-pathname :pukunui "resouces/ev.wav"))))
-  (setf (sample-loop-p ev) nil)
-  (setf (sample-playing-p ev) t)
-  (setf (unit-gain ev) 0.8)
-  (pukunui:init ev))
+(progn
+  (setf (sample-idx *ev*) 0)
+  (setf (sample-loop-p *ev*) nil)
+  (setf (sample-playing-p *ev*) t)
+  (setf (unit-gain *ev*) 0.8)
+  (pukunui:init *ev*))
 
 ;; play a WAV file ad infinitum
-(let ((ev (read-wav (asdf:system-relative-pathname :pukunui "resouces/ev.wav"))))
-  (setf (sample-loop-p ev) t)
-  (setf (sample-playing-p ev) t)
-  (setf (unit-gain ev) 0.8)
-  (pukunui:init ev))
+(progn
+  (setf (sample-idx *ev*) 0)
+  (setf (sample-loop-p *ev*) t)
+  (setf (sample-playing-p *ev*) t)
+  (setf (unit-gain *ev*) 0.8)
+  (pukunui:init *ev*)
 
 ;; play two samples
-(let ((ev (read-wav (asdf:system-relative-pathname :pukunui "resouces/ev.wav")))
-      (dr (read-wav (asdf:system-relative-pathname :pukunui "resouces/dr.wav")))
-      (mixer (create-umix)))
-  (setf (sample-loop-p ev) t)
-  (setf (sample-playing-p ev) t)
-  (setf (unit-gain ev) 0.8)
+(let ((mixer (create-umix)))
+  (setf (sample-idx *ev*) 0)
+  (setf (sample-loop-p *ev*) t)
+  (setf (sample-playing-p *ev*) t)
+  (setf (unit-gain *ev*) 0.8)
 
-  (setf (sample-loop-p dr) t)
-  (setf (sample-playing-p dr) t)
-  (setf (unit-gain dr) 0.8)
+  (setf (sample-idx *dr*) 0)
+  (setf (sample-loop-p *dr*) t)
+  (setf (sample-playing-p *dr*) t)
+  (setf (unit-gain *dr*) 0.8)
 
-  (setf (unit-src mixer) (vector ev dr))
+  (setf (unit-src mixer) (vector *ev* *dr*))
   (pukunui:init mixer))
--- examples/03-fx.lisp
+++ examples/03-fx.lisp
@@ -9,19 +9,23 @@
                :pukunui/units/sample
                :pukunui/event))
 
+(defparameter *dr*
+  (read-wav (asdf:system-relative-pathname :pukunui "resouces/dr.wav")))
+
 ;; play a drum sample without delay
-(let ((dr (read-wav (asdf:system-relative-pathname :pukunui "resouces/dr.wav"))))
-  (setf (sample-loop-p dr) t)
-  (setf (sample-playing-p dr) t)
-  (setf (unit-gain dr) 0.8)
-  (pukunui:init dr))
+(progn
+  (setf (sample-idx *dr*) 0)
+  (setf (sample-loop-p *dr*) t)
+  (setf (sample-playing-p *dr*) t)
+  (setf (unit-gain *dr*) 0.8)
+  (pukunui:init *dr*))
 
 ;; play a drum sample with delay
-(let ((dr (read-wav (asdf:system-relative-pathname :pukunui "resouces/dr.wav")))
-      (delay (create-delay-1* 0.5 0.2)))
-  (setf (sample-loop-p dr) t)
-  (setf (sample-playing-p dr) t)
-  (setf (unit-gain dr) 0.8)
+(let ((delay (create-delay-1* 0.5 0.2)))
+  (setf (sample-idx *dr*) 0)
+  (setf (sample-loop-p *dr*) t)
+  (setf (sample-playing-p *dr*) t)
+  (setf (unit-gain *dr*) 0.8)
 
-  (setf (unit-src delay) dr)
+  (setf (unit-src delay) *dr*)
   (pukunui:init delay))
