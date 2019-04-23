a4bce54b43e158983376acc1b66abf8e65d862ac
t-sin <shinichi.tanaka45@gmail.com>
2019-04-23 10:15:37 +0900

    More sample for seq sample

--- examples/04-seq.lisp
+++ examples/04-seq.lisp
@@ -41,6 +41,27 @@
     (,(make-timepos :bar 4 :beat 0 :pos 0) :on ,(note->freq 72))
     (,(make-timepos :bar 4 :beat 3 :pos 0) :off)))
 
+(defparameter *bass-seq*
+  (flet ((seq1 (bar beat n1 n2)
+           (list (list (make-timepos :bar bar :beat beat :pos 0) :on (note->freq n1))
+                 (list (make-timepos :bar bar :beat beat :pos 0.5) :on (note->freq n2))
+                 (list (make-timepos :bar bar :beat beat :pos 0.75) :off)
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0) :on (note->freq n2))
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0.25) :off)
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0.5) :on (note->freq n2))
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0.75) :off)))
+         (seq2 (bar beat n)
+           (list (list (make-timepos :bar bar :beat beat :pos 0) :on (note->freq n))
+                 (list (make-timepos :bar bar :beat beat :pos 0.25) :off)
+                 (list (make-timepos :bar bar :beat beat :pos 0.5) :on (note->freq n))
+                 (list (make-timepos :bar bar :beat beat :pos 0.75) :off)
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0) :on (note->freq n))
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0.25) :off)
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0.5) :on (note->freq n))
+                 (list (make-timepos :bar bar :beat (1+ beat) :pos 0.75) :off))))
+    (append (seq1 1 0 48 52) (seq2 1 2 52) (seq1 2 0 48 52) (seq2 2 2 52)
+            (seq1 3 0 50 53) (seq1 3 2 43 47) (seq1 4 0 48 52) (seq2 4 2 52))))
+
 (defparameter *snare-seq*
   `((,(make-timepos :bar 0 :beat 3 :pos 0) :on)
     (,(make-timepos :bar 0 :beat 3 :pos 0.5) :on)
@@ -71,7 +92,8 @@
 
 ;; kb2
 (let* ((seq1 (create-useq* *kb-seq* 30 30000 0.2 0))
-       (seq2 (create-unseq* *snare-seq* 30 3000 0 0))
+       (seq2 (create-useq* *bass-seq* 30 10000 0.5 0))
+       (seq3 (create-unseq* *snare-seq* 30 3000 0 0))
        (mixer (create-umix)))
-  (setf (unit-src mixer) (vector seq1 seq2))
+  (setf (unit-src mixer) (vector seq1 seq2 seq3))
   (pukunui:init (create-unit mixer 0.2 0) 150))
